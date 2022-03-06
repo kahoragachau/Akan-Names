dc7c48043df9cacc13b84d1670d41071c9088e86
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-06 20:19:01 +0300

    remove border from css

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,31 @@ let submitBtn = document.getElementById('submit-button');
 submitBtn.addEventListener("click", akanName);
 
 function akanName(event){
-    let year = document.getElementById('year').value;
-    console.log(year);
-    if(year === ""){
-        console.log("Please enter an year")
+    let dd = document.getElementById('birthDate').value;
+    if(dd === ""){
+        alert("Date input cant be empty");
+    }
+    if(dd > 31 || dd <= 0){
+        alert("Enter a Valid Date!!");
     }
 
     let mm = document.getElementById('monthDate').value;
-    console.log(mm);
     if(mm === ""){
-        console.log("Please enter a month")
+        alert("Month Input Can't be empty")
     }
     if(mm > 12 || mm <=0 ){
-        console.log("Enter a Valid Month!!")
+        alert("Enter a Valid Month!!")
     }
-    let dd = document.getElementById('birthDate').value;
-    console.log(dd);
-    if(dd === ""){
-        console.log("Please enter a day")
+
+    let year = document.getElementById('year').value;
+    if(year === ""){
+        alert("Year Input Can't be empty")
     }
-    if(dd > 31 || dd <= 0){
-        console.log("Enter a Valid Date!!")
-    }
+
     
     let birthDay = dd + '/' + mm + '/' + year;
-    console.log(birthDay);
-
     let dateOfBirth = new Date(birthDay);
-    console.log(dateOfBirth);
-
     let dayOfWeek = dateOfBirth.getDay();
-    console.log(dayOfWeek)
 
     let gender = document.getElementsByName('gender');
     let days = ["Sunday","Monday", "Tuesday", "Wednesday","Thurday","Friday","Saturday"];
