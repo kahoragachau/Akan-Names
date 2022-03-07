e5cc8f71895fd79e28474dd16aae8007a6758509
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-07 08:48:47 +0300

    Fixed the date issue from dd/mm/yy to mm/dd/yy

--- js/scripts.js
+++ js/scripts.js
@@ -22,11 +22,17 @@ function akanName(event){
     if(year === ""){
         document.getElementById("error-message-5").innerHTML = "<span style='color:red'>Year Input Can't Be Empty</span>";
     }
+    // let cc = year.slice(0,2);
+    // let yy = year.slice(2,);
 
     
-    let birthDay = dd + '/' + mm + '/' + year;
+    // let weekDay = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
+    // console.log(weekDay)
+
+    let birthDay = mm + '/' + dd + '/' + year;
     let dateOfBirth = new Date(birthDay);
     let dayOfWeek = dateOfBirth.getDay();
+    console.log(dateOfBirth)
 
     let gender = document.getElementsByName('gender');
     let days = ["Sunday","Monday", "Tuesday", "Wednesday","Thurday","Friday","Saturday"];
