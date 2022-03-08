4da7a746e4430149cfdf4cd1947c75c7ab5339ba
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-08 21:59:50 +0300

    Add validators

--- js/scripts.js
+++ js/scripts.js
@@ -3,31 +3,26 @@ submitBtn.addEventListener("click", akanName);
 
 function akanName(event){
     let dd = document.getElementById('birthDate').value;
-    if(dd == ""){
-        document.getElementById("error-message-1").innerHTML = "<span style='color:red'>Date Input Can't Be Empty</span>";
-    }
-    if(dd > 31 || dd <= 0){
-        document.getElementById("error-message-2").innerHTML = "<span style='color:red'>Enter a number between 1 to 31</span>";
-    }
-
     let mm = document.getElementById('monthDate').value;
-    if(mm === ""){
-        document.getElementById("error-message-3").innerHTML = "<span style='color:red'>Month Input Can't Be Empty</span>";
-    }
-    if(mm > 12 || mm <=0 ){
-        document.getElementById("error-message-4").innerHTML = "<span style='color:red'>Enter Month Between 1 to 12</span>";
-    }
-
     let year = document.getElementById('year').value;
-    if(year === ""){
-        document.getElementById("error-message-5").innerHTML = "<span style='color:red'>Year Input Can't Be Empty</span>";
-    }
-    // let cc = year.slice(0,2);
-    // let yy = year.slice(2,);
 
-    
-    // let weekDay = ( ( (cc/4) -2*cc-1) + ((5*yy/4) ) + ((26*(mm+1)/10)) + dd ) % 7
-    // console.log(weekDay)
+    let notValidInput = true
+
+    if(dd === ""){
+        document.getElementsByClassName("error-message-1")[0].innerHTML = "<span style='color:red'>Date Input Can't Be Empty</span>";
+    }
+    else if(dd > 31 || dd <= 0){
+        document.getElementsByClassName("error-message-2")[0].innerHTML = "<span style='color:red'>Enter a number between 1 to 31</span>";
+    }
+    if(mm === ""){
+        document.getElementsByClassName("error-message-1")[1].innerHTML = "<span style='color:red'>Month Input Can't Be Empty</span>";
+    }
+    else if(mm > 12 || mm <=0 ){
+        document.getElementsByClassName("error-message-2")[1].innerHTML = "<span style='color:red'>Enter Month Between 1 to 12</span>";
+    }
+    if(year === ""){
+        document.getElementsByClassName("error-message-1")[2].innerHTML = "<span style='color:red'>Year Input Can't Be Empty</span>";
+    }
 
     let birthDay = mm + '/' + dd + '/' + year;
     let dateOfBirth = new Date(birthDay);
@@ -40,7 +35,7 @@ function akanName(event){
     let femaleNames = ["Akosua", "Adwoa", "Abenaa","Akua","Yaa","Afua","Ama"];
 
     for(let i = 0; i<gender.length; i++){
-        if(gender[i].checked){
+        if(gender[i].checked && (dd !== "" && !(dd > 31 || dd <= 0))&& (mm !== "" && !(mm > 12 || mm <=0)) && year!== ""){
             if(gender[i].id === "male-radio"){
                 alert("Your Born on a " + days[dayOfWeek] + " Your Akan Name is " + maleNames[dayOfWeek])
             }
